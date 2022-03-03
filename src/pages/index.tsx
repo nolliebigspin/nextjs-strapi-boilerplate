f12ab6ab61cf0376e5100c97825e68e9ae774e61
nolliebigspin <alec96.w@gmail.com>
2022-03-03 23:26:36 +0100

    Move everything to src dir

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,7 @@ import type { NextPage } from "next";
 import { Center, Square, Circle, Box, Flex } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
-  return (
-    <Center>
-      <Box as="span" fontWeight="bold" fontSize="lg" bg="tomato" p="100" mt="100">
-        IN PROGRESS!
-      </Box>
-    </Center>
-  );
+  return <div></div>;
 };
 
 export default Home;
