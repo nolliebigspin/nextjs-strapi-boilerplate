f10d9941b853e24652596dabf4bad5f83a307897
nolliebigspin <alec96.w@gmail.com>
2022-03-06 00:07:49 +0100

    Setup Monorepo with Lerna including strapi and frontend

--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import { Center, Square, Circle, Box, Flex } from "@chakra-ui/react";
-
-const Home: NextPage = () => {
-  return <div></div>;
-};
-
-export default Home;
