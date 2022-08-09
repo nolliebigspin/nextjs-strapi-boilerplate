ad1b5e4d1eb83108dd0d0557f10174b98419dccf
nolliebigspin <alec007@t-online.de>
2022-08-09 22:50:15 +0200

    Update apolloClient config

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    STRAPI_HOST: process.env.STRAPI_HOST,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
