d2abadcba170a9d74965d9d2137d7f1b33f37dea
nolliebigspin <alec007@t-online.de>
2023-02-14 18:29:45 +0100

    Add eslint prettier + all needed dependecies

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
-  root: true,
-  extends: ["custom"],
-};
+  plugins: ["@typescript-eslint", "jsx-a11y"],
+  extends: [
+    "next/core-web-vitals",
+    "prettier",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jsx-a11y/recommended"
+  ],
+  rules: {
+    "@typescript-eslint/ban-ts-comment": "warn",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"]
+      }
+    ]
+  }
+}
